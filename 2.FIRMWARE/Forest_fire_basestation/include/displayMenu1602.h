8e979e1f86bbe97edf07dd659236b1981c07bebe
Nischal_M <nischal.m.edu@gmail.com>
2021-04-16 01:30:55 +0530

    added GSM; LORA;

---
+++ 2.FIRMWARE/Forest_fire_basestation/include/displayMenu1602.h
@@ -0,0 +1,14 @@
+#ifndef _DISPLAY_MENU_1602_H
+#define _DISPLAY_MENU_1602_H
+#include "Arduino.h"
+
+class displayMenu1602
+{
+
+public:
+    void begin()
+    {
+    }
+};
+
+#endif //_DISPLAY_MENU_1602_H
