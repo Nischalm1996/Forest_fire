20322aea8e56fb21c7300b7a111e6f9f1d7aae25
Nischalm1996 <nischal.m.edu@gmail.com>
2021-04-16 23:39:34 +0530

    created pir.h

---
+++ 2.FIRMWARE/Forest_fire_node/include/PIR.h
@@ -0,0 +1,26 @@
+/*
+1. begin
+2. boolean detect()
+*/
+
+#ifndef _PIR_H
+#define _PIR_H
+#include "Arduino.h"
+const int8 pirPin;
+class PIR
+{
+    public:
+    void begin(int pin)
+    {
+        pirPin = pin;
+        pinMode(pin,INPUT);
+    }
+
+    boolean detect()
+    {
+        return(digitalRead(pirPin));
+    }
+
+
+};
+#endif //_PIR_H
